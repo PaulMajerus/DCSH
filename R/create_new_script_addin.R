55ddc92656b10b100666b6693213a5607eb2d883
PaulMajerus <majerus.paul@protonmail.com>
2025-05-13 20:37:25 +0200

    showprompt for addin

--- R/create_new_script_addin.R
+++ R/create_new_script_addin.R
@@ -1,13 +1,31 @@
-#' Addin pour créer un script R standardisé
+#' Add-in interactif pour créer un script standardisé
 #'
-#' @return Ouvre un script dans le répertoire courant avec structure type
+#' @return Crée un fichier script dans le répertoire courant
 #' @export
 create_new_script_addin <- function() {
-  rstudioapi::showDialog("Création script",
-                         "Un script structuré va être créé dans le répertoire courant.")
-  scriptTemplate(
-    auteur = "Paul Majérus",
-    titre = "",
-    fileName = "nouveau_script"
+  if (!rstudioapi::isAvailable()) {
+    stop("RStudio API non disponible.")
+  }
+
+  # Demander les valeurs à l'utilisateur
+  titre <- rstudioapi::showPrompt(title = "Titre", message = "Titre du script", default = "Titre")
+  if (is.null(titre)) return(invisible(NULL))
+
+  fileName <- rstudioapi::showPrompt(title = "Nom de fichier", message = "Nom du fichier (sans extension)", default = "nouveau_script")
+  if (is.null(fileName)) return(invisible(NULL))
+
+  auteur <- rstudioapi::showPrompt(title = "Auteur", message = "Nom de l'auteur", default = Sys.info()[["user"]])
+  if (is.null(auteur)) return(invisible(NULL))
+
+  version <- rstudioapi::showPrompt(title = "Version", message = "Version du script",
+                                    default = 1)
+  if (is.null(auteur)) return(invisible(NULL))
+
+  # Appel à ta fonction principale
+  create_new_script(
+    auteur = auteur,
+    titre = titre,
+    fileName = fileName,
+    version = version
   )
 }
