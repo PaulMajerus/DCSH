ad1eb15ac4e6174fedc15a01a2aa0a6f86462b85
PaulMajerus <majerus.paul@protonmail.com>
2025-05-23 23:10:48 +0200

    Modification nightsCount

--- R/nightsCount.R
+++ R/nightsCount.R
@@ -3,38 +3,37 @@
 #' Computes the number of nights between two dates, based on the difference in days.
 #' Dates must be provided as character strings in the same specified format.
 #'
-#' @param startDate A character string representing the start date (e.g., "20230501").
-#' @param endDate A character string representing the end date (e.g., "20230505").
-#' @param format A character string specifying the date format, in `as.Date()` style.
+#' @param startDate A character string of length 12 representing the start date - time (e.g., "202305011200").
+#' @param endDate A character string of length 12 representing the end date - time (e.g., "202305052315").
 #'
 #' @return An integer representing the number of nights between \code{startDate} and \code{endDate}.
 #'         If \code{startDate == endDate}, the result is 0. If \code{endDate < startDate}, the result is negative.
 #'
 #' @export
-#'
+#' @import lubridate
+#' @import stringr
 #' @examples
-#' nightsCount("20230501", "20230505")
-#' nightsCount("20230501", "20230501")
-#' nightsCount("20230505", "20230501")
+#' nightsCount("202305011150", "202305051155")
+#' nightsCount("202305011230", "202305010152")
+#' nightsCount("202305051300", "202305010000")
 nightsCount <- function(startDate = character(0),
-                        endDate = character(0),
-                        format = "%Y%m%d"){
+                        endDate = character(0)){
 
   # Vérification de la présence des dates
-  if (nchar(startDate) == 0 || nchar(endDate) == 0) {
-    stop("Nor startDate nor endDate can be empty.")
+  if (nchar(startDate) < 8 || nchar(endDate) < 8) {
+    stop("startDate and endDate should be length 8 or more.")
   }
 
   # Convert start date and end date to date format
-  start <- as.Date(startDate, format = format)
-  end <- as.Date(endDate, format = format)
+  start <- lubridate::ymd(stringr::str_sub(startDate,1,8))
+  end <- lubridate::ymd(stringr::str_sub(endDate,1,8))
 
   if (is.na(start) || is.na(end)) {
     stop("startDate or endDate is invalid regarding the format.")
   }
 
   # Compute nights count
-  diff <- as.numeric(difftime(end, start, units = "days"))
+  diff <- as.numeric(end-start)
 
   return(diff)
 
--- tests/testthat/test-nightsCount.R
+++ tests/testthat/test-nightsCount.R
@@ -2,37 +2,31 @@
 
   # Test 1: Vérifier que le calcul des nuits est correct (exemple simple)
   test_that("Calcul des nuits entre deux dates est correct", {
-    result <- nightsCount("20230501", "20230505", format = "%Y%m%d")
+    result <- nightsCount("202305012210", "202305051105")
     expect_equal(result, 4)  # 5 mai - 1 mai = 4 nuits
   })
 
   # Test 2: Vérifier que le calcul des nuits donne 0 quand les dates sont identiques
   test_that("Le calcul des nuits est 0 quand les dates sont identiques", {
-    result <- nightsCount("20230501", "20230501", format = "%Y%m%d")
+    result <- nightsCount("202305011115", "202305012359")
     expect_equal(result, 0)
   })
 
   # Test 3: Vérifier que l'argument 'startDate' ou 'endDate' vide déclenche une erreur
   test_that("Erreur quand startDate ou endDate est vide", {
-    expect_error(nightsCount("", "20230505", format = "%Y%m%d"), "Nor startDate nor endDate can be empty.")
-    expect_error(nightsCount("20230501", "", format = "%Y%m%d"), "Nor startDate nor endDate can be empty.")
+    expect_error(nightsCount("", "202305"), "startDate and endDate should be length 8 or more.")
+    expect_error(nightsCount("202305011215", ""), "startDate and endDate should be length 8 or more.")
   })
 
   # Test 4: Vérifier une erreur quand le format de date est invalide
   test_that("Erreur si startDate ou endDate est invalide selon le format", {
-    expect_error(nightsCount("20230501", "invalid_date", format = "%Y%m%d"), "startDate or endDate is invalid regarding the format.")
-    expect_error(nightsCount("invalid_date", "20230505", format = "%Y%m%d"), "startDate or endDate is invalid regarding the format.")
-  })
-
-  # Test 5: Vérifier les dates avec un autre format (par exemple : %Y-%m-%d)
-  test_that("Le calcul fonctionne avec un format de date différent", {
-    result <- nightsCount("2023-05-01", "2023-05-05", format = "%Y-%m-%d")
-    expect_equal(result, 4)
+    expect_error(nightsCount("202305011212", "invalid_date"), "startDate or endDate is invalid regarding the format.")
+    expect_error(nightsCount("invalid_date", "202305050000"), "startDate or endDate is invalid regarding the format.")
   })
 
   # Test 6: Vérifier le cas où endDate est avant startDate
   test_that("Le calcul des nuits donne un résultat négatif quand endDate < startDate", {
-    result <- nightsCount("20230505", "20230501", format = "%Y%m%d")
+    result <- nightsCount("202305051210", "202305011210")
     expect_equal(result, -4)  # 1er mai - 5 mai = -4 nuits
   })
 #
